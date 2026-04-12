56a596123fed2745614dfec0735b80563970d311
Vladimir <nucluster@gmail.com>
2026-04-12 12:30:09 +0500

    Update settings.py with CSRF_TRUSTED_ORIGINS

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -13,6 +13,8 @@ DEBUG = int(os.getenv('DEBUG', default=1))
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", default='localhost 127.0.0.1').split()
 
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS").split()
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
