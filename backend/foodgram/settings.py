e7fc0500454604df83258c24d6b3274f3850b84d
Vladimir <nucluster@gmail.com>
2026-04-12 20:27:48 +0500

    Update settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -13,7 +13,7 @@ DEBUG = int(os.getenv('DEBUG', default=1))
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", default='localhost 127.0.0.1').split()
 
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS").split()
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", default='').split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
